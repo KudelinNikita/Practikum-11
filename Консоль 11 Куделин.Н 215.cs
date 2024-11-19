22363e8be367872ec473642cf37e2d77dc71fe8f
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-19 13:22:42 +0200

    Add files via upload

---
+++ Консоль 11 Куделин.Н 215.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Приактикум_11_Куделин.Н_215гр
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАЧА 1.Вывод  сообщений приветствия и прощания с использованием дополнительных методов.
+
+            //    SayHello();
+            //    Bye();
+            //    Console.ReadKey();
+
+            //ЗАДАЧА 2.Нахождение суммы, разности, произведения и частного двух целых чисел с использованием
+            //дополнительных методов.
+
+            //Console.WriteLine("Введите первое число:");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите второе число:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"\nСумма: {Add(num1, num2)}");
+            //Console.WriteLine($"Разность: {Subtract(num1, num2)}");
+            //Console.WriteLine($"Произведение: {Multiply(num1, num2)}");
+            //Console.WriteLine($"Частное: {Divide(num1, num2)}");
+            //Console.WriteLine("\nНажмите Enter для завершения...");
+            //Console.ReadKey();
+
+            //ЗАДАЧА 3.Вычисление значения функции:y={4x^2+1/x-5, если x<5.
+                                                  //={3x^2-2, если x>=5.
+        }
+        //метод задачи 1
+        //        public static void SayHello()
+        //{
+        //    Console.WriteLine("Здарова");
+        //}
+        //public static void Bye()
+        //{
+        //    Console.WriteLine("Адиос");
+        //}
+
+        //метод задачи 2
+        //static int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+        //static int Subtract(int a, int b)
+        //{
+        //    return a - b;
+        //}
+        //static int Multiply(int a, int b)
+        //{
+        //    return a * b;
+        //}
+        //static double Divide(int a, int b)
+        //{
+        //    if (b == 0)
+        //    {
+        //        Console.WriteLine("Ошибка: Деление на ноль невозможно.");
+        //        return double.NaN;
+        //    }
+        //    return (double)a / b;
+        //    }
+    }
+}
